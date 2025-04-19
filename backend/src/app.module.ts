a48531d7a4f08b803c8b1b6ca53f7f04aa9a3f27
umesh kumar <umeshc0509@gmail.com>
2025-04-20 02:59:01 +0530

    switched DB to postgreSQl with DATABASE_URL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,36 @@
-// src/app.module.ts
+// // src/app.module.ts
+// import { Module } from '@nestjs/common';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { OrdersModule } from './orders/orders.module';
+// import { SocketModule } from './socket/socket.module';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true, // makes config available globally
+//     }),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       inject: [ConfigService],
+//       useFactory: (config: ConfigService) => ({
+//         type: 'mysql',
+//         host: config.get<string>('DB_HOST'),
+//         port: config.get<number>('DB_PORT'),
+//         username: config.get<string>('DB_USERNAME'),
+//         password: config.get<string>('DB_PASSWORD'),
+//         database: config.get<string>('DB_NAME'),
+//         entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//         synchronize: true,
+//       }),
+//     }),
+//     OrdersModule,
+//     SocketModule,
+//   ],
+// })
+// export class AppModule {}
+
+
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,20 +40,19 @@ import { SocketModule } from './socket/socket.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // makes config available globally
+      isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true, // recommended for NestJS
         synchronize: true,
+        ssl: {
+          rejectUnauthorized: false, // Required for some Render databases
+        },
       }),
     }),
     OrdersModule,
@@ -29,3 +60,4 @@ import { SocketModule } from './socket/socket.module';
   ],
 })
 export class AppModule {}
+
